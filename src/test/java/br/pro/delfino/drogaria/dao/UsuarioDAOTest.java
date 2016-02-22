ea0d3d4e351ef1b466ef36ab52fe8ca1b8da0977
Dyego <dyego.eti@gmail.com>
2016-02-22 11:34:36 -0300

    Implementação do método de teste salvar da classe UsuarioDAO

--- src/test/java/br/pro/delfino/drogaria/dao/UsuarioDAOTest.java
+++ src/test/java/br/pro/delfino/drogaria/dao/UsuarioDAOTest.java
@@ -1,5 +1,30 @@
 package br.pro.delfino.drogaria.dao;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.pro.delfino.drogaria.domain.Pessoa;
+import br.pro.delfino.drogaria.domain.Usuario;
+
 public class UsuarioDAOTest {
+	
+	@Test
+	@Ignore
+	public void salvar() {
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		Long codigoPessoa = 1L;
+		Pessoa pessoa = pessoaDAO.buscar(codigoPessoa);
+		
+		Usuario usuario = new Usuario();
+		usuario.setSenha("123456");
+		usuario.setTipo('A');
+		usuario.setAtivo(true);
+		usuario.setPessoa(pessoa);
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuarioDAO.salvar(usuario);
+		
+		System.out.println("Usuário salvo com sucesso.");
+	}
 
 }
